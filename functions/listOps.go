8782ce76c311bc53e139beb14a77ea950ebb1a46
Haraldur Steinar Skúlason <halli@hallis20.me>
2024-12-14 19:19:55 +0000

    moved map,zip,reduce to listOps

--- functions/listOps.go
+++ functions/listOps.go
@@ -6,6 +6,41 @@ import (
 	"strings"
 )
 
+func Reduce[T, U any](list []T, f func(T, U) U) U {
+	switch len(list) {
+	case 0:
+		panic("Too few elements to reduce")
+	case 1:
+		var zero U
+		return f(list[0], zero)
+	default:
+		return f(list[0], Reduce(list[1:], f))
+	}
+}
+
+func Map[T, U any](list []T, f func(T) U) []U {
+	switch len(list) {
+	case 0:
+		return []U{}
+	case 1:
+		return []U{f(list[0])}
+	default:
+		return append([]U{f(list[0])}, Map(list[1:], f)...)
+	}
+}
+
+func Zip[T, U, V any](list1 []T, list2 []U, f func(T, U) V) []V {
+	if len(list1) != len(list2) {
+		panic("list lengths don't match")
+	}
+	switch len(list2) {
+	case 1:
+		return []V{f(list1[0], list2[0])}
+	default:
+		return append([]V{f(list1[0], list2[0])}, Zip(list1[1:], list2[1:], f)...)
+	}
+}
+
 func SplitList(list []string, seperator string) [][]string {
 	newList := [][]string{}
 	for i := 0; i < len(list); i++ {
